eb16cec6bd30e0a975cdafb31b48bf65b70a6d1b
Cyrhina <cyrenemlumabas@gmail.com>
2026-01-04 15:28:59 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "", // dev
+  base: "/final-wmit-llda/", // dev
   build: {
     chunkSizeWarningLimit: 15000,
   },
