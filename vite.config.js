315625341c7781a80d3960052985ba606b8b7932
Cyrhina <86034766+Cyrhina@users.noreply.github.com>
2026-01-06 21:40:56 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/final-wmit-llda/", // dev
+  base: "/", // dev
   build: {
     chunkSizeWarningLimit: 15000,
   },
